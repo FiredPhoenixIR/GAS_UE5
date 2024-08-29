4c35b52961274c8fa8cecf5bb6cd813f9c2d5918
Mahdi ST <mahdi.sadeghian.tehrani@gmail.com>
2024-08-29 18:48:10 +0330

    Base Char Setup

--- Source/GAS/Public/MST_BaseCharacter.h
+++ Source/GAS/Public/MST_BaseCharacter.h
@@ -4,26 +4,57 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
 #include "MST_BaseCharacter.generated.h"
 
 UCLASS()
-class GAS_API AMST_BaseCharacter : public ACharacter
+class GAS_API AMST_BaseCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AMST_BaseCharacter();
 
 protected:
-	// Called when the game starts or when spawned
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS", Meta = (AllowPrivateAcess = true))
+	class UAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS", Meta = (AllowPrivateAcess = true))
+	class UMST_HealthSet* HealthSet;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "GAS")
+	TArray<TSubclassOf<class UMST_GameAbility>> DefaultAbilities;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "GAS")
+	TArray<TSubclassOf<class UGameplayEffect>> DefaultEffects;
+
+protected:
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
-	// Called to bind functionality to input
+	virtual void InitializeAbilities();
+	virtual void InitializeEffects();
+
+	virtual void PostInitializeComponents() override;
+
+	virtual void OnDamageTakenChanged(AActor* DamageInstigator, AActor* DamageCauser, const FGameplayTagContainer& GameplayTagConstainer, float Damage);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "GAS")
+	void OnDamageTaken(AActor* DamageInstigator, AActor* DamageCauser, const FGameplayTagContainer& GameplayTagConstainer, float Damage);
+
+	virtual void OnHealthAttributeChanged(const FOnAttributeChangeData& Data);	
+	
+	UFUNCTION(BlueprintImplementableEvent, Category = "GAS")
+	void OnHealthChanged(float OldValue, float NewValue);
+
+	virtual void OnShieldAttributeChanged(const FOnAttributeChangeData& Data);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "GAS")
+	void OnShieldChanged(float OldValue, float NewValue);
+public:	
+	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
