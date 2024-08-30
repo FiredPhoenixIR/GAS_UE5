4ba3b38e61cd3b20d879778971d8f99600ec4bf8
Mahdi ST <mahdi.sadeghian.tehrani@gmail.com>
2024-08-30 19:09:57 +0330

    character setup

--- Source/GAS/Private/MST_PlayerCharacter.cpp
+++ Source/GAS/Private/MST_PlayerCharacter.cpp
@@ -2,4 +2,41 @@
 
 
 #include "MST_PlayerCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+#include "Net/UnrealNetwork.h"
+#include "AbilitySystemComponent.h"
+#include "MST_HealthSet.h"
 
+
+// Sets default values
+AMST_PlayerCharacter::AMST_PlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+	GetCharacterMovement()->MaxWalkSpeed = 300.f;
+	GetCharacterMovement()->SetIsReplicated(true);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f); // Yaw is rotation against Z
+
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 300.f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+	ViewCamera->SetupAttachment(SpringArm);
+}
--- Source/GAS/Public/MST_PlayerCharacter.h
+++ Source/GAS/Public/MST_PlayerCharacter.h
@@ -6,6 +6,15 @@
 #include "MST_BaseCharacter.h"
 #include "MST_PlayerCharacter.generated.h"
 
+
+class UCameraComponent;
+class USpringArmComponent;
+class UGroomComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+
 /**
  * 
  */
@@ -13,5 +22,38 @@ UCLASS()
 class GAS_API AMST_PlayerCharacter : public AMST_BaseCharacter
 {
 	GENERATED_BODY()
+
+	AMST_PlayerCharacter();
 	
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	UCameraComponent* ViewCamera;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	USpringArmComponent* SpringArm;
+
+protected:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* MappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookingAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly , Category = "Input")
+	UInputAction* FireAbilityAction;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SendAbilityLocalInput(const FInputActionValue& Value, int32 InpudID);
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void FireAbility(const FInputActionValue& Value);
+
 };
